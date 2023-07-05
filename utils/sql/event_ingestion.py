593a8959bc658078f36e9c4f47b092dcc52907d5
rtanoeiro <ramontanoeiro@gmail.com>
2023-07-05 12:10:25 +1000

    Added event ingestion and reports

--- utils/sql/event_ingestion.py
+++ utils/sql/event_ingestion.py
@@ -1,1 +1,12 @@
 from base_queries import EVENTS
+import pandas as pd
+from sqlalchemy import create_engine
+from config.dev import host, database, user, password, table_name
+
+connection_string = f"postgresql://{user}:{password}@{host}/{database}"
+engine = create_engine(connection_string)
+
+data = pd.read_csv("data/platform_transactions.csv")
+data.to_sql(table_name, engine, if_exists="append", index=False)
+
+engine.dispose()
--- utils/sql/reports.py
+++ utils/sql/reports.py
@@ -1,1 +1,10 @@
 from base_queries import FINANCE_QUERY, MARKETING_QUERY
+import pandas as pd
+from sqlalchemy import create_engine
+from config.dev import host, database, user, password, table_name
+
+connection_string = f"postgresql://{user}:{password}@{host}/{database}"
+engine = create_engine(connection_string)
+
+finance_report = pd.read_sql(FINANCE_QUERY, engine)
+marketing_report = pd.read_sql(MARKETING_QUERY, engine)
