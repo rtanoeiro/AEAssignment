7959c648e47aa117f97a70df01cb1e315d1abef4
rtanoeiro <ramontanoeiro@gmail.com>
2023-07-05 12:37:03 +1000

    Updated mock_data and added transformation file

--- utils/sql/event_ingestion.py
+++ utils/sql/event_ingestion.py
@@ -2,11 +2,14 @@ from base_queries import EVENTS
 import pandas as pd
 from sqlalchemy import create_engine
 from config.dev import host, database, user, password, table_name
+from transformations import fill_customer_country, fill_gender
 
 connection_string = f"postgresql://{user}:{password}@{host}/{database}"
 engine = create_engine(connection_string)
 
 data = pd.read_csv("data/platform_transactions.csv")
+data = fill_customer_country(data)
+data = fill_gender(data)
 data.to_sql(table_name, engine, if_exists="append", index=False)
 
 engine.dispose()
