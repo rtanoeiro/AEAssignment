17d1bfa86df194717a4c86ec4adcf2e540279d03
rtanoeiro <ramontanoeiro@gmail.com>
2023-07-05 12:38:05 +1000

    Changed reports to be saved as csv

--- utils/sql/reports.py
+++ utils/sql/reports.py
@@ -6,5 +6,7 @@ from config.dev import host, database, user, password, table_name
 connection_string = f"postgresql://{user}:{password}@{host}/{database}"
 engine = create_engine(connection_string)
 
-finance_report = pd.read_sql(FINANCE_QUERY, engine)
-marketing_report = pd.read_sql(MARKETING_QUERY, engine)
+finance_report = pd.read_sql(FINANCE_QUERY, engine).to_csv("data/finance_report.csv")
+marketing_report = pd.read_sql(MARKETING_QUERY, engine).to_csv(
+    "data/marketing_report.csv"
+)
