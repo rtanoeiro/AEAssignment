6453daf4b5175beb9de867f7e92ef90ee27c3461
aletheavilla <thea.villa@indebted.co>
2023-07-06 14:26:47 +0800

    edit var name

--- utils/aws/aws_functions.py
+++ utils/aws/aws_functions.py
@@ -5,9 +5,9 @@ def get_daily_data(download_from, download_to):
     """
     Get daily data from S3 bucket
     """
-    s3_resource = boto3.client("s3")
+    s3_client = boto3.client("s3")
 
-    s3_resource.download_file(
+    s3_client.download_file(
         Bucket=bucket_name, Key=download_from, Filename=download_to
     )
 
@@ -16,8 +16,8 @@ def upload_reports(upload_from, upload_to):
     """
     Upload reports to S3 bucket
     """
-    s3_resource = boto3.client("s3")
+    s3_client = boto3.client("s3")
 
-    s3_resource.upload_file(
+    s3_client.upload_file(
         Bucket=bucket_name, Filename=upload_from, Key=upload_to
     )
