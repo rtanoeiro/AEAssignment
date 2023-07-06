9a6e4091ed4703a41e1f389419d778b8821dc9f8
aletheavilla <thea.villa@indebted.co>
2023-07-06 09:14:05 +0800

    shorten s3 functions

--- utils/aws/aws_functions.py
+++ utils/aws/aws_functions.py
@@ -6,9 +6,9 @@ def get_daily_data(download_from, download_to):
     """
     Get daily data from S3 bucket
     """
-    s3_resource = boto3.resource("s3")
+    s3_resource = boto3.client("s3")
 
-    s3_resource.meta.client.download_file(
+    s3_resource.download_file(
         Bucket=bucket_name, Key=download_from, Filename=download_to
     )
 
@@ -17,8 +17,8 @@ def upload_reports(upload_from, upload_to):
     """
     Upload reports to S3 bucket
     """
-    s3_resource = boto3.resource("s3")
+    s3_resource = boto3.client("s3")
 
-    s3_resource.meta.client.upload_file(
+    s3_resource.upload_file(
         Bucket=bucket_name, Filename=upload_from, Key=upload_to
     )
