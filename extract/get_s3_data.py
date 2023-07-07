752e34bab8b0cfd4c7898c9c70d7812ff6080a3e
Rache Melendres <rachemelendres@gmail.com>
2023-07-08 02:46:40 +0800

    combined s3 connection in the download_file function

--- extract/get_s3_data.py
+++ extract/get_s3_data.py
@@ -1,4 +1,5 @@
 from clients.s3 import access_s3
 
-def download_file(s3_client, bucket_name, s3_file_path, s3_download_file_path):
+def download_file(bucket_name, s3_file_path, s3_download_file_path):
+  s3_client = access_s3()
   s3_client.download_file(Bucket=bucket_name, Key=s3_file_path, Filename=s3_download_file_path)
