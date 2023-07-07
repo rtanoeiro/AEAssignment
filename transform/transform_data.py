c854857f4ab175d03be698a824341a6be511406f
Rache Melendres <rachemelendres@gmail.com>
2023-07-08 01:47:09 +0800

    updated code on transforming data

---
+++ transform/transform_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def read_data(data_filepath):
+  data = pd.read_csv(data_filepath)
+  return data
+
+def fill_customer_country(data):
+    return data
+
+def fill_gender(data):
+    return data
