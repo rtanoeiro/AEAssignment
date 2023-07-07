ee517c949bccc99bd6ac519559a2a6c4788fa8d3
Rache Melendres <rachemelendres@gmail.com>
2023-07-08 01:23:50 +0800

    added extract phase on the pipeline

--- main_new.py
+++ main_new.py
@@ -1,7 +1,16 @@
 from config.config import bucket_name, data_folder
+from clients.s3 import access_s3
+from extract.get_s3_data import download_file
 import os
 
 s3_file_name = 'platform_transactions.csv'
 s3_file_path = f'{data_folder}/{s3_file_name}'
 working_dir = os.getcwd()
 s3_download_file_path = os.path.join(working_dir, s3_file_name)
+
+def main():
+  s3_client = access_s3()
+  download_file(s3_client, bucket_name, s3_file_path,s3_download_file_path)
+  
+if __name__ == "__main__":
+    main()
