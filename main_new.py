79c4d0165f1f1aba6c72b0fd0b7c3765688b807d
Rache Melendres <rachemelendres@gmail.com>
2023-07-08 02:58:28 +0800

    updated transformation code

--- main_new.py
+++ main_new.py
@@ -1,16 +1,30 @@
-from config.config import bucket_name, data_folder
-from clients.s3 import access_s3
+from config.config import bucket_name, data_folder, host, database, user, password, table_name
+from clients.data_lake import connect_data_lake
 from extract.get_s3_data import download_file
+from transform.transform_data import apply_transformations
 import os
 
 s3_file_name = 'platform_transactions.csv'
 s3_file_path = f'{data_folder}/{s3_file_name}'
-working_dir = os.getcwd()
+working_dir = os.path.join(os.getcwd(),'data')
 s3_download_file_path = os.path.join(working_dir, s3_file_name)
 
+
 def main():
-  s3_client = access_s3()
-  download_file(s3_client, bucket_name, s3_file_path,s3_download_file_path)
+
+# Extract S3 Data
+  download_file(bucket_name, s3_file_path,s3_download_file_path)
+
+ # Transform Data 
   
+  transformed_data = apply_transformations(s3_download_file_path)
+  
+  
+ # Load Data
+  connect_data_lake(user,password,host,database)
+ 
+ # Generate Reports
+ 
+
 if __name__ == "__main__":
     main()
--- transform/transform_data.py
+++ transform/transform_data.py
@@ -4,6 +4,12 @@ def read_data(data_filepath):
   data = pd.read_csv(data_filepath)
   return data
 
+def apply_transformations(extracted_data):
+  data_df = read_data(extracted_data)
+  transformed_data_df1 = fill_customer_country(data_df)
+  transformed_data_df2 = fill_gender(transformed_data_df1)
+  return transformed_data_df2 
+  
 def fill_customer_country(data):
     return data
 
