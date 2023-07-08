e0bf8c82defd71579c05a1b93377fb9d967f4c37
Rache Melendres <rachemelendres@gmail.com>
2023-07-08 14:28:40 +0800

    rearranged order of functions and fixed variable names

--- transform/transform_data.py
+++ transform/transform_data.py
@@ -4,14 +4,14 @@ def read_data(data_filepath):
   data = pd.read_csv(data_filepath)
   return data
 
-def apply_transformations(extracted_data):
-  data_df = read_data(extracted_data)
-  transformed_data_df1 = fill_customer_country(data_df)
-  transformed_data_df2 = fill_gender(transformed_data_df1)
-  return transformed_data_df2 
-  
 def fill_customer_country(data):
     return data
 
 def fill_gender(data):
     return data
+  
+def apply_transformations(extracted_data_filepath):
+  extracted_data = read_data(extracted_data_filepath)
+  transformed_data = fill_customer_country(extracted_data)
+  transformed_data = fill_gender(transformed_data)
+  return transformed_data
