cede14d80a8569ab957b7a9b58568b064a8e91ab
Rache Melendres <rachemelendres@gmail.com>
2023-07-08 13:42:05 +0800

    included a reports list to loop over

---
+++ report/reports.py
@@ -0,0 +1,14 @@
+from utils.sql.base_queries import FINANCE_QUERY, MARKETING_QUERY
+
+reports_list = [
+    {
+        "department": "finance",
+        "query": FINANCE_QUERY,
+      
+    },
+    {
+        "department": "marketing",
+        "query": MARKETING_QUERY,
+    },
+   
+]
